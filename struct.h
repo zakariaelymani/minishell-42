fe7deea2ade4f62b9d2dda7809016afffe8fd536
Zakaria El Yamani <zel-yama@e2r8p9.1337.ma>
2025-05-17 16:34:41 +0100

    EE

--- struct.h
+++ struct.h
@@ -6,7 +6,7 @@
 /*   By: zel-yama <zel-yama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:46:34 by zel-yama          #+#    #+#             */
-/*   Updated: 2025/05/15 14:58:38 by zel-yama         ###   ########.fr       */
+/*   Updated: 2025/05/17 16:31:31 by zel-yama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,33 @@ typedef struct s_env
 	
 }   t_env;
 
-//this redirction  type 
-typedef enum  s_redir
-{
-	INPUT,
-	OUTPUT,
-	APPEND,
-	HER_DOC,
-	
-}   t_redir;
+//this redirection  type 
+/*typedef enum  s_redir*/
+/*{*/
+/*	INPUT,*/
+/*	OUTPUT,*/
+/*	APPEND,*/
+/*	HER_DOC,*/
+/**/
+/*}   t_redir;*/
 
-typedef struct s_redir_struct
+typedef enum e_type{
+	WORD = 1 << 0,
+	APPEND = 1 << 1,
+	HEREDOC = 1 << 2,
+	INPUT = 1 << 3,
+	OUTPUT = 1 << 4,
+	PIPE = 1 << 5,
+	NONE = 1 << 6,
+} t_type;
+
+typedef struct s_redir
 {
 	char *file_name; // if here_doc   this well be limiter 
-	t_redir type; //<< >> < > 
+	t_type type; //<< >> < > 
 	int 	fd;/// this form my work don't touch this 
-	struct  s_redir_struct *next;
-}	t_redir_s;
+	struct  s_redir *next;
+}	t_redir;
 
 typedef struct s_cmds
 {
@@ -53,7 +63,7 @@ typedef struct s_cmds
 	int			input;
 	int 		output;//this for me 
 	pid_t 		pid;
-	t_redir_s 	*redirction;//this put strcut to  s
+	t_redir 	*redirection;//this put strcut to  s
 	struct		s_cmds *next;
 }	t_cmds;
 
