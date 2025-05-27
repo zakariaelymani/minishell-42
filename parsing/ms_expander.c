37a1e7ed1ea84ac2a46b1d3c707138109d8463f9
aymane <abenkaro@student.1337.ma>
2025-05-27 03:32:50 +0100

    expanding...

--- parsing/ms_expander.c
+++ parsing/ms_expander.c
@@ -12,15 +12,13 @@
 
 #include "parsing.h"
 
-size_t	expand_size(t_token *tokens)
+size_t	expanded_size(char *str)
 {
 	size_t	len;
 	int		expand;
-	char	*str;
 
 	len = 0;
 	expand = 1;
-	str = tokens->content;
 	while (*str)
 	{
 		if (*str == '\'')
@@ -35,3 +33,56 @@ size_t	expand_size(t_token *tokens)
 	}
 	return (len);
 }
+
+int	expand(char *str, char **env)
+{
+	size_t	len;
+	int		expand;
+	char	*result;
+	int		i;
+
+	len = expanded_size(str);
+	i = 0;
+	expand = 1;
+	result = malloc(len + 1);
+	if (!result)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '\'')
+			expand =! expand;
+		if (str[i] == '$' && expand)
+		{
+			copy_from_env(result, &i, env);
+			continue ;
+		}
+		i++;
+	}
+	free(str);
+	str = result;
+	return (1);
+}
+
+int	ms_expander(t_token *tokens, char **env)
+{
+	int		err;
+	t_token	*head;
+
+	head = tokens;
+	while (head)
+	{
+		head = head->next;
+		err = expand(head->content, env);
+		if (err < 0)
+		{
+			ft_putstr_fd("Allocation Failure", 2);
+			ms_tokclear(tokens);
+			return (0);
+		}
+	}
+	return (1);
+}
+
+
+
+
