86c2a4c5ccce9edf9dbb4eaf9d13e4fcc998cfd6
aymane <abenkaro@student.1337.ma>
2025-06-20 23:04:18 +0100

    ok

--- parsing/split_manager.c
+++ parsing/split_manager.c
@@ -12,11 +12,47 @@
 
 #include "parsing.h"
 
-static void	inject_split(char *s)
+// static void	inject_split(char *s)
+// {
+// 	while (*++s)
+// 		if (*s == '\x1D')
+// 			*s = '\x1F';
+// }
+
+void split_keyvalue(char *eq)
 {
-	while (*++s)
-		if (*s == '\x1D')
-			*s = '\x1F';
+	int both;
+	char *p1;
+	char *p2;
+
+	p1 = eq;
+	p2 = p1;
+	both = 0;
+	while (*p1 && *p1 != '\x1F')
+		if (*--p1 == '\x1D')
+			both++;
+	while (*p2 && *p2!= '\x1F')
+		if (*++p2== '\x1D')
+			both++;
+	if (both > 1)
+	{
+		while (*p1 && *p1 != '=')
+		{
+			if (*p1 == '\x1D')
+				*p1 = '\x1F';
+			p1++;
+		}
+		while (*p2 && *p2 != '=')
+		{
+			if (*p2 == '\x1D')
+				*p2 = '\x1F';
+			p2--;
+		}
+	}
+	else
+		while (*++eq && *eq != '\x1F')
+			if (*eq == '\x1D')
+				*eq = ' ';
 }
 
 void	split_manager(char *s)
@@ -35,15 +71,13 @@ void	split_manager(char *s)
 			close = ft_strchr(close, '=');
 			if (!close)
 				break ;
-			while (*++close != '\x1F')
-				if (*close == '\x1D')
-					*close = ' ';
+			split_keyvalue(close);
 		}
-		inject_split(s);
+		// inject_split(s);
 	}
 	else
 	{
 		*close = '\x1F';
-		inject_split(s);
+		// inject_split(s);
 	}
 }
